244d5c57df00406e9f5a28894b6c68c522cf33b0
tomeido <tomeido@naver.com>
2023-02-03 16:25:59 +0900

    still working

---
+++ MeshToJSON/App.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+
+namespace MeshToJSON
+{
+    public class App : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabname = "JSON";
+            string panelname = "Mesh to JSON";
+            //creating the bitimages
+            BitmapImage btn1image = new BitmapImage(new Uri(@"C:\RevitAddin\MeshToJSON\MeshToJSON\Resources\32_JSONicon01.png"));
+
+            //creating the tab
+            application.CreateRibbonTab(tabname);
+
+            //creating the panel
+            var addinpanel = application.CreateRibbonPanel(tabname, panelname);
+
+            //creating the button
+            var button1 = new PushButtonData("Mesh to JSON button1", "Mesh to JSON", Assembly.GetExecutingAssembly().Location, "MeshToJSON.Command");
+            button1.ToolTip = "JSON";
+            button1.LongDescription = "JSON";
+            button1.LargeImage = btn1image;
+
+            var btn1 = addinpanel.AddItem(button1) as PushButton;
+
+            return Result.Succeeded;
+        }
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ MeshToJSON/Command.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+using System.Windows.Forms;
+using System.IO;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.DB.Mechanical;
+using System.Diagnostics;
+
+namespace MeshToJSON
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Command : IExternalCommand
+    {
+        XYZ vertex1 = new XYZ();
+        XYZ vertex2 = new XYZ();
+        XYZ vertex3 = new XYZ();
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get application and document objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            try
+            {
+
+                //Pick a group
+                Selection sel = uiapp.ActiveUIDocument.Selection;
+          
+                Reference faceRef = sel.PickObject(ObjectType.Face, "Pick Face");
+                GeometryObject a = doc.GetElement(faceRef).GetGeometryObjectFromReference(faceRef);
+                Face f = a as Face;
+
+                GetTrianglesFromFace(f);
+                string str = vertex1.ToString() + vertex2.ToString() + vertex3.ToString();
+                TaskDialog.Show("vertex", str);
+
+                return Result.Succeeded;
+            }
+
+            //If the user right-clicks or presses Esc, handle the exception
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
+        }
+
+        private void GetTrianglesFromFace(Face face)
+        {
+            // Get mesh
+            Mesh mesh = face.Triangulate();
+            for (int i = 0; i < mesh.NumTriangles; i++)
+            {
+                MeshTriangle triangle = mesh.get_Triangle(i);
+                vertex1 = triangle.get_Vertex(0);
+                vertex2 = triangle.get_Vertex(1);
+                vertex3 = triangle.get_Vertex(2);
+            }
+        }
+
+    }
+}
